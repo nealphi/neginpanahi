894461248f21d2ed8bc4106c9849bd06e36b3de2
Negin Alphi <negin.alipanahi@gmail.com>
2024-07-12 12:29:36 -0400

    mini projects deleted

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,37 +24,6 @@ const Projects = () => {
         features animations from Framer Motion. Deployed on Vercel for a smooth
         and reliable experience.
       </ProjectLayout>
-      <ProjectLayout
-        href="https://to-do-app-react-nealphis-projects.vercel.app/"
-        title={"To-Do-List Application"}
-      >
-        Developed a minimal To-Do tracking application featuring advanced
-        filtering options to manage tasks efficiently. Designed in Figma, coded
-        with React and deployed with Vercel.
-      </ProjectLayout>
-      <ProjectLayout
-        href="https://github.com/nealphi/Clock"
-        title={"Analog Clock Mini Project"}
-      >
-        Created an animated analog clock utilizing vanilla JavaScript and CSS
-        for coding and styling. The clock features dynamic movement of hour,
-        minute, and second hands, providing a real-time display of the current
-        time. The project showcases fundamental JavaScript techniques for
-        handling time updates and CSS skills for designing and animating the
-        clock's visual elements.Animated analog clock coded and styled with
-        vanilla JavaScript and CSS.
-      </ProjectLayout>
-      <ProjectLayout
-        href="https://github.com/nealphi/Flip-a-Coin"
-        title={"Coin Flip Mini Project"}
-      >
-        Developed a coin flip project using vanilla JavaScript and CSS. The
-        project features a coin that flips with a realistic animation and
-        randomly displays heads or tails. JavaScript handles the logic and event
-        listeners for the flip, while CSS is used to design the coin and create
-        smooth animations. The user can interact with the application by
-        clicking a button to flip the coin and view the result.
-      </ProjectLayout>
     </Flex>
   );
 };
