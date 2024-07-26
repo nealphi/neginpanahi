12bc001436fdafd0fe0dec5a218adc99221dba75
Negin Alphi <negin.alipanahi@gmail.com>
2024-07-25 20:07:17 -0400

    skills component updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,18 +1,44 @@
 import React from "react";
 import SplineAnimation from "./SplineAnimation";
-import { Box, Flex } from "@chakra-ui/react";
+import { Flex, Grid, Image } from "@chakra-ui/react";
 import SkillsTags from "./SkillsTags";
 import Tools from "./Tools";
 
 const Skills = () => {
   return (
-    <Box className="skills" gap={10}>
-      <SplineAnimation />
-      <Flex justifyContent={'center'} gap={'20%'} paddingX={['2%','10%']} wrap={'wrap'}>
+    <Grid
+      className="skills"
+      templateColumns={{
+        sm: "repeat(1, 1fr)",
+        md: "repeat(1, 1fr)",
+        lg: "repeat(2, 1fr)",
+      }}
+      gap={10}
+      paddingX={["0", "10%"]}
+    >
+      <Flex
+        position={"relative"}
+        flexDirection={"column"}
+        alignItems={"center"}
+      >
+        <SplineAnimation />
+        <Image
+          width={["40px"]}
+          position={"absolute"}
+          bottom={"20px"}
+          display={{ base: "none", md: "block" }}
+          src="./swipe.png"
+        />
+      </Flex>
+      <Flex
+        justifyContent={"center"}
+        paddingX={["2%", "10%"]}
+        wrap={"wrap"}
+      >
         <SkillsTags />
         <Tools />
       </Flex>
-    </Box>
+    </Grid>
   );
 };
 
